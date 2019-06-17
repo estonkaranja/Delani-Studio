e23382c1101883f82cb9bf2e4d3963b03f23da76
estonkaranja <estonkaranja881@gmail.com>
2019-06-17 11:43:58 +0300

    added new commits

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $(".designhidden, .designshowing").click(function () {
+    $(".designshowing").toggle();
+    $(".designhidden").toggle();
+  });
+  $(".devhidden, .devshowing").click(function () {
+    $(".devshowing").toggle();
+    $(".devhidden").toggle();
+  });
+  $(".producthidden, .productshowing").click(function () {
+    $(".productshowing").toggle();
+    $(".producthidden").toggle();
+  });
+  $("#hovers1").hover(function(){
+    }, function(){
+    $(this).css("background-color", "white");
+  });
+    $("#hovers2").hover(function(){
+      }, function(){
+      $(this).css("background-color", "white");
+  });
+    $("#hovers3").hover(function(){
+      }, function(){
+      $(this).css("background-color", "white");
+  });
+      $("#submit").click(function () {
+        alert("Your message has been received!")
+      })
+  })
